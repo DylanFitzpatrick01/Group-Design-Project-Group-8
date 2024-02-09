49f2001b9309be7fdd2971a98994eb1e4a733edf
Dylan Fitzpatrick <dylanfitzpatrick33@gmail.com>
2024-02-09 15:41:06 +0000

    added login page

--- frontend/src/components/NewPage.js
+++ frontend/src/components/NewPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
-import '../styles/NewPage.css'; // import the CSS file
+import './NewPage.css'; // import the CSS file
 
 function NewPage() {
   const [getMessage, setGetMessage] = useState({})
