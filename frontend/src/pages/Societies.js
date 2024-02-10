d3bf099c928012720442c86c35590c36ddb6a0f9
edurnin <72498252+edurnin@users.noreply.github.com>
2024-02-10 17:19:23 +0000

    Added Societies page

--- frontend/src/pages/Societies.js
+++ frontend/src/pages/Societies.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
+import '../styles/Societies.css'; // import the CSS file
 
-import NewPage from './pages/NewPage'; // import NewPage
-import Societies from './pages/Societies'; // import Societies page
-
-
-function App() {
+function Societies() {
   const [getMessage, setGetMessage] = useState({})
 
-  useEffect(() => {
+  useEffect(()=>{
     axios.get('http://localhost:5000/flask/hello').then(response => {
       console.log("SUCCESS", response)
       setGetMessage(response)
@@ -20,13 +15,16 @@ function App() {
 
   }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <NewPage /> {}
+    <div className="Societies">
+      <header className="Societies-header">
+        <p>Login</p>
+        <div>{getMessage.status === 200 ? 
+          <h3>{getMessage.data.message}</h3>
+          :
+          <h3>LOADING</h3>}</div>
       </header>
     </div>
   );
 }
 
-export default App;
+export default Societies;
