09d27292c2914f0d8ee643e476206102b2396980
Jacky HE <ihkk.nb@gmail.com>
2024-02-12 00:02:50 +0000

    add skeleton of Profile

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
+import './Profile.css'; // import the CSS file
 
-import NewPage from './components/NewPage'; // import NewPage
-import Profile from './components/Profile'; // import Profile
-
-function App() {
+function Profile() {
   const [getMessage, setGetMessage] = useState({})
 
   useEffect(() => {
@@ -19,13 +15,16 @@ function App() {
 
   }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <NewPage /> { }
+    <div className="Profile">
+      <header className="Profile-header">
+        <p>React + Flask Tutorial</p>
+        <div>{getMessage.status === 200 ?
+          <h3>{getMessage.data.message}</h3>
+          :
+          <h3>LOADING</h3>}</div>
       </header>
     </div>
   );
 }
 
-export default App;
+export default Profile;
