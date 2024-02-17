dec9643dce5122de7f775c9b320b4ab2f00e46ea
Jacky HE <ihkk.nb@gmail.com>
2024-02-17 19:01:35 +0000

    change firebase.js to multi exports

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -15,8 +15,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-
-export default db;
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const db = getFirestore(app);
