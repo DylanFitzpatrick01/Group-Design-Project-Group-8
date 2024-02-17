0b0d5ce6bc15b80b354c5dc373a88903c2a8d36b
HaoyuWww <wangh8@tcd.ie>
2024-02-17 00:19:45 +0000

    create firebase.js and set up firestore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,24 +10,6 @@ import NewPage from './components/NewPage'; // import NewPage
 import Societies from './components/Societies'; // import Societies page
 import NotificationsPage from './components/NotificationsPage/NotificationsPage';
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDAlrmNP4IydBXFKbj9ry7fZQmrswg1HKk",
-  authDomain: "group-8---college-social-media.firebaseapp.com",
-  projectId: "group-8---college-social-media",
-  storageBucket: "group-8---college-social-media.appspot.com",
-  messagingSenderId: "833481597283",
-  appId: "1:833481597283:web:eeb23af113f6b5965bb356",
-  measurementId: "G-CB5M15DR9W"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
 function App() {
 
   return (
