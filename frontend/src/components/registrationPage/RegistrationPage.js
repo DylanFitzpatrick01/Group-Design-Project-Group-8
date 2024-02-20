fe7a1774dfebb819cb37b9d52fd01358cf148802
Dylan Fitzpatrick <dylanfitzpatrick33@gmail.com>
2024-02-20 16:17:22 +0000

    Data is now stored to firebase on submit. year of study is dropdown

--- frontend/src/components/registrationPage/RegistrationPage.js
+++ frontend/src/components/registrationPage/RegistrationPage.js
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import './RegistrationPage.css'; // import the CSS file
+import { collection, addDoc } from "firebase/firestore";
+import {db} from '../../firebase';
 
 function RegistrationPage() {
-  const [getMessage, setGetMessage] = useState({});
-  const [image, setImage] = useState({});
+  const [getMessage, setGetMessage] = useState({})
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    courseTitle: '',
+    yearOfStudy: '1',
+    bio: '',
+    image: null,
+  });
+
+  const emailRegex = /^[^\s@]+@tcd\.ie$/i; // Regex to validate TCD email
 
   useEffect(()=>{
     axios.get('http://localhost:5000/flask/hello').then(response => {
@@ -21,9 +33,46 @@ function RegistrationPage() {
     if (selectedImage) {
       const reader = new FileReader();
       reader.onload = () => {
-        setImage(reader.result);
+        setFormData({ ...formData, image: reader.result });
       };
       reader.readAsDataURL(selectedImage);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleYearChange = (e) => {
+    setFormData({...formData, yearOfStudy: e.target.value});
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!emailRegex.test(formData.email)) {
+      setFormData({ ...formData, emailError: 'Must use a valid TCD email address' });      return;
+    }
+    else{
+      setFormData({ ...formData, emailError: '' }); 
+    }
+
+    try {
+      const docRef = await addDoc(collection(db, 'users'), formData);
+      console.log('Document written with ID: ', docRef.id);
+
+      setFormData({
+        email: '',
+        firstName: '',
+        lastName: '',
+        image: {},
+        yearOfStudy: '',
+        courseTitle: '',
+        bio: '',
+      });
+    } catch (error) {
+      console.error('Error adding document: ', error);
     }
   };
 
@@ -32,52 +81,63 @@ function RegistrationPage() {
       <header className="RegistrationPage-header">
         <h1>Registration</h1>
       </header>
-      <div className = "RegistrationBox">
-          <div className="ProfilePicture">
-            <p className="ProfilePictureText">Profile Picture</p>
-            <img src={image} alt="Preview" className="PreviewImage"/>
-            <input className="ImageUpload" type="file" accept="image/*" onChange={handleImageChange} />
-        </div>
-        <div className = "row row-cols-2">
-          <div className="col">
-            <div className= "FirstName">
-              <p>First Name</p>
-              <input required className="FirstNameInput" />
-            </div>
+      <form onSubmit={handleSubmit}>
+        <div className = "RegistrationBox">
+            <div className="ProfilePicture">
+              <p className="ProfilePictureText">Profile Picture</p>
+              <img src={formData.image} alt="Preview" className="PreviewImage"/>
+              <input className="ImageUpload" type="file" accept="image/*" onChange={handleImageChange} />
           </div>
-          <div className="col">
-            <div className= "LastName">
-              <p>Surname</p>
-              <input required className="LastNameInput" />
+          <div className = "row row-cols-2">
+            <div className="col">
+              <div className= "FirstName">
+                <p>First Name</p>
+                <input required className="FirstNameInput" name="firstName" value={formData.firstName} onChange={handleInputChange}/>
+              </div>
             </div>
-          </div>
-          <div className =" col">
-            <div className = "Email">
-              <p>TCD Email Address</p>
-              <input required className="EmailInput"/>
+            <div className="col">
+              <div className= "LastName">
+                <p>Surname</p>
+                <input required className="LastNameInput" name="lastName" value = {formData.lastName} onChange={handleInputChange}/>
+              </div>
             </div>
-          </div>
-          <div className = "col">
-            <div className= "CourseName">
-              <p>Course Title</p>
-              <input required className="CourseNameInput" />
+            <div className =" col">
+              <div className = "Email">
+                <p>TCD Email Address</p>
+                <input required className="EmailInput" name="email" value = {formData.email} onChange={handleInputChange}/>
+                {formData.emailError && <p className="ErrorText">{formData.emailError}</p>}
+              </div>
             </div>
-          </div>
-          <div className='col'>
-            <div className= "YearOfStudy">
-              <p>Year Of Study</p>
-              <input required className="YearOfStudyInput" />
+            <div className = "col">
+              <div className= "CourseName">
+                <p>Course Title</p>
+                <input required className="CourseNameInput" name="courseTitle" value = {formData.courseTitle} onChange={handleInputChange}/>
+              </div>
+            </div>
+            <div className='col'>
+            <div className="YearOfStudy" onChange={handleYearChange}>
+                <p>Year of Study</p>
+                <select value={formData.yearOfStudy} className="YearOfStudyInput">
+                  <option value="1">Junior Freshman (Year 1)</option>
+                  <option value="2">Senior Freshman (Year 2)</option>
+                  <option value="3">Junior Sophister (Year 3)</option>
+                  <option value="4">Senior Sophister (Year 4)</option>
+                  <option value="5">Masters</option>
+                  <option value="6">PhD</option>
+                  <option value="7">Other</option>
+                </select>
+              </div>  
             </div>
           </div>
           <div className= "Bio">
             <p>Bio</p>
-            <textarea className="BioInput" />
+            <textarea className="BioInput" name="bio" value = {formData.bio} onChange={handleInputChange} />
           </div>
         </div>
-      </div>
-      <button className="SubmitButton">
-        Submit
-      </button>
+        <button type = "submit" className="SubmitButton">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
