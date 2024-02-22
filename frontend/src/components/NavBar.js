b42bf2b7e1c643ff8ef7572eddb810d149f04415
edurnin <72498252+edurnin@users.noreply.github.com>
2024-02-22 17:45:33 +0000

    Indicates what page is displayed

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = ({ navLinks }) => {
+    const location = useLocation();
     return (
         <nav className="navbar">
           <h1> Trinity College Dublin  </h1>
           <ul>
             {navLinks.map((link, index) => (
               <li key={index}>
-                <Link to={link.to}>{link.label}</Link>
+                <Link to={link.to} className={location.pathname === link.to ? "active" : ""}>{link.label}</Link>
               </li>
             ))}
           </ul>
