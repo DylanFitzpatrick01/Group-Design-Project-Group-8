ce8dc2faedf6d8359b7cd509d86d18b766a24363
Haojun6 <99478305+Haojun6@users.noreply.github.com>
2024-02-28 00:13:04 +0000

    search-modules-from-database-and-add-to-page

--- frontend/src/components/module/ModuleList.js
+++ frontend/src/components/module/ModuleList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../../firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import {collection, getDocs, doc, getDoc, setDoc, updateDoc, arrayUnion, onSnapshot} from 'firebase/firestore';
 import { Link, useNavigate } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import './ModuleList.css';
@@ -10,8 +10,9 @@ import AddChatCollections from './AddChatCollections';
 const ModulesList = () => {
   const [modules, setModules] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
-  const [myModule, setmyModule] = useState([]);
-  const modulesCollectionRef = collection(db, "modules");
+  const [myModule, setMyModule] = useState([]);
+  const modulesCollectionRef = collection(db, 'modules');
+  const auth = getAuth();
 
   // if the user is not logged in, redirect to the login page
   const navigate = useNavigate();
@@ -19,13 +20,19 @@ const ModulesList = () => {
     const auth = getAuth();
     const unsubscribe = onAuthStateChanged(auth, user => {
       if (!user) {
-        console.log("User is not logged in");
+        console.log('User is not logged in');
         navigate('/');
+      } else {
+        fetchUserFavorites(user);
       }
     });
-
     return () => unsubscribe();
-  }, []);
+  }, [auth, navigate]);
+
+  const fetchModules = async () => {
+    const data = await getDocs(modulesCollectionRef);
+    setModules(data.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+  };
 
   useEffect(() => {
     fetchModules();
@@ -35,58 +42,75 @@ const ModulesList = () => {
     const addChats = async () => {
       await AddChatCollections();
     };
-
     addChats();
   }, []);
 
-  const fetchModules = async () => {
-    const data = await getDocs(modulesCollectionRef);
-    setModules(data.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+  const fetchUserFavorites = (user) => {
+    const userFavoritesRef = doc(db, 'userFavorites', user.email.split("@")[0]);
+    onSnapshot(userFavoritesRef, (doc) => {
+      if (doc.exists()) {
+        setMyModule(doc.data().modules || []);
+      } else {
+        console.log('No user document');
+      }
+    });
   };
 
   const handleSearch = (searchTerm) => {
     const results = modules.filter(module =>
       module.id.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      module.name.toLowerCase().includes(searchTerm.toLowerCase())
+      module.name.toLowerCase().includes(searchTerm.toLowerCase()),
     );
     setSearchResults(results);
   };
 
-  const handleAddTomyModule = (moduleToAdd) => {
-    setmyModule(prevList => {
-      // Check if the module is already in the list to avoid duplicates
-      const isAlreadyAdded = prevList.some(module => module.id === moduleToAdd.id);
-      if (!isAlreadyAdded) {
-        return [...prevList, moduleToAdd];
+  const handleAddTomyModule = async (moduleToAdd) => {
+    if (!auth.currentUser) {
+      console.log('No user logged in');
+      return;
+    }
+
+    const user = auth.currentUser;
+    const userFavoritesRef = doc(db, 'userFavorites', user.email.split("@")[0]);
+    await updateDoc(userFavoritesRef, {
+      modules: arrayUnion(moduleToAdd),
+    }).catch(async (error) => {
+      if (error.code === 'not-found') {
+        await setDoc(userFavoritesRef, { modules: [moduleToAdd] });
       }
-      return prevList;
     });
   };
 
   return (
     <div>
-      <div className="content-container">
-        <div className="my-modules">
+      <div className='content-container'>
+        <div className='my-modules'>
           <h2>My Modules</h2>
-          {myModule.map((module) => (
+          {myModule.map(module => (
             <Link key={module.id} to={`/modules/${module.id}`}>
-              <button key={module.id} className="module-item-button">
+              <button key={module.id} className='module-item-button'>
                 <div>{module.name}</div>
                 <div>{module.id}</div>
               </button>
             </Link>
           ))}
         </div>
-        <div className="search-section">
+        <div className='search-section'>
           <AddModuleBar onSearch={handleSearch} />
           {searchResults.length > 0 && (
-            <div className="search-results"><br/><br/><br/>
+            <div className='search-results'><br/><br/><br/>
               <h2>&#8195;Results:</h2>
-              {searchResults.map((module) => (
-                <div key={module.id} className="search-result-item">
+              {searchResults.map(module => (
+                <div key={module.id} className='search-result-item'>
                   <div>{module.name}</div>
-                  <div>{module.id}&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;
-                  <button className="add-button" onClick={() => handleAddTomyModule(module)}>Add</button>
+                  <div>
+                    {module.id}&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;
+                    <button
+                      className='add-button'
+                      onClick={() => handleAddTomyModule(module)}
+                    >
+                      Add
+                    </button>
                   </div>
                 </div>
               ))}
@@ -95,7 +119,7 @@ const ModulesList = () => {
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default ModulesList;
