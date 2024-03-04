8a664d8ffee7d26e6abdabb24409308350061e32
HaoyuWww <wangh8@tcd.ie>
2024-03-04 17:28:38 +0000

    Modified sendChat function to accept an optional imageUrl parameter.

--- frontend/src/components/chatPage/SendReceiveChats.js
+++ frontend/src/components/chatPage/SendReceiveChats.js
@@ -12,21 +12,19 @@ import {
 // add new chat to a module's chat collection
 async function sendChat(moduleCode, text, user, imageUrl = null) {
     try {
-        // 构建消息对象，如果提供了imageUrl，则包括这个字段
         const messageData = {
             displayName: user.name,
-            text: text, // 文本内容，可以为空如果是仅发送图片
+            text: text, 
             timestamp: serverTimestamp(),
             uid: user.email,
             avatar: user.avatar,
         };
 
-        // 如果提供了图片URL，将其添加到消息数据中
+        // If the image URL is provided, add it to the message data
         if (imageUrl) {
             messageData.imageUrl = imageUrl;
         }
-
-        // 将消息对象添加到对应模块的聊天集合中
+        //Adds the message object to the chat collection of the corresponding module
         await addDoc(collection(db, 'modules', moduleCode, 'chats'), messageData);
     } catch (error) {
         console.error("Failed to send chat:", error);
