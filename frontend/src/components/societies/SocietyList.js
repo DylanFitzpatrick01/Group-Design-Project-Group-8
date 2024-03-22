d3a04806b6e7ebd0f7cf46b1a4ed7e7893d42d00
Haojun6 <99478305+Haojun6@users.noreply.github.com>
2024-03-22 02:26:15 +0000

    add search society bar

--- frontend/src/components/societies/SocietyList.js
+++ frontend/src/components/societies/SocietyList.js
@@ -1,64 +1,123 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../../firebase';
-import { collection, getDocs } from 'firebase/firestore';
-import './SocietyList.css'; // import the CSS file
-import { Link } from 'react-router-dom';
-import AddSocietyChatCollections from './AddSocietyChatCollections';
+import {collection, getDocs, doc, getDoc, setDoc, updateDoc, arrayUnion, onSnapshot} from 'firebase/firestore';
+import { Link, useNavigate } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
+import './SocietyList.css';
+import AddSocietyBar from './AddSocietyBar';
+import AddSocietyChatCollections from './AddSocietyChatCollections';
 
-
-const SocietiesList = () => {
+const SocietyList = () => {
   const [societies, setSocieties] = useState([]);
-  const socieitesCollectionRef = collection(db, "societies");
+  const [searchResults, setSearchResults] = useState([]);
+  const [mySociety, setMySociety] = useState([]);
+  const societiesCollectionRef = collection(db, 'societies');
+  const auth = getAuth();
+
+  // if the user is not logged in, redirect to the login page
   const navigate = useNavigate();
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (!user) {
+        console.log('User is not logged in');
+        navigate('/');
+      } else {
+        fetchUserFavorites(user);
+      }
+    });
+    return () => unsubscribe();
+  }, [auth, navigate]);
+
+  const fetchSocieties = async () => {
+    const data = await getDocs(societiesCollectionRef);
+    setSocieties(data.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+  };
 
   useEffect(() => {
-    const getSocieties = async () => {
-      const data = await getDocs(socieitesCollectionRef);
-      setSocieties(data.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-    };
-
-    getSocieties();
+    fetchSocieties();
   }, []);
 
   useEffect(() => {
     const addChats = async () => {
       await AddSocietyChatCollections();
     };
-
     addChats();
-  }, [])
-  
-  useEffect(() => {
-  const auth = getAuth();
-    const unsubscribe = onAuthStateChanged(auth, user => {
-      if (!user) {
-        console.log("User is not logged in");
-        navigate('/');
-      }
-    });
-
-    return () => unsubscribe();
   }, []);
 
-  
+  const fetchUserFavorites = (user) => {
+    const userFavoritesRef = doc(db, 'userFavorites', user.email.split("@")[0]);
+    onSnapshot(userFavoritesRef, (doc) => {
+      if (doc.exists()) {
+        setMySociety(doc.data().societies || []);
+      } else {
+        console.log('No user document');
+      }
+    });
+  };
 
+  const handleSearch = (searchTerm) => {
+    const results = societies.filter(society =>
+      society.id.toLowerCase().includes(searchTerm.toLowerCase())
+      ,
+    );
+    setSearchResults(results);
+  };
+
+  const handleAddTomySociety = async (societyToAdd) => {
+    if (!auth.currentUser) {
+      console.log('No user logged in');
+      return;
+    }
+
+    const user = auth.currentUser;
+    const userFavoritesRef = doc(db, 'userFavorites', user.email.split("@")[0]);
+    await updateDoc(userFavoritesRef, {
+      societies: arrayUnion(societyToAdd),
+    }).catch(async (error) => {
+      if (error.code === 'not-found') {
+        await setDoc(userFavoritesRef, { societies: [societyToAdd] });
+      }
+    });
+  };
 
   return (
     <div>
-      <div className="societies-list-container">
-        <h1 className="societies-title">Societies List</h1>
-        {societies.map((society) => (
+      <div className='content-container'>
+        <div className="societies-list-container">
+        <h2 className="societies-title">Societies List</h2>
+        {mySociety.map((society) => (
           <Link key={society.id} to={`/societies/${society.id}`}>
             <button className="society-item-button">
               {society.id}
             </button>
           </Link>
         ))}
+        </div>
+        <div className='search-section'>
+          <AddSocietyBar onSearch={handleSearch} />
+          {searchResults.length > 0 && (
+            <div className='search-results'><br/><br/><br/>
+              <h2>&#8195;Results:</h2>
+              {searchResults.map(society => (
+                <div key={society.id} className='search-result-item'>
+                  <div>
+                    {society.id}<br/>&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;
+                    <button
+                      className='add-button'
+                      onClick={() => handleAddTomySociety(society)}
+                    >
+                      Add
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
 };
 
-export default SocietiesList;
+export default SocietyList;
