ec7ce1372e0f0a84f00a2bd703939754950f6137
HaoyuWww <wangh8@tcd.ie>
2024-04-06 19:05:41 +0100

    Added handleDMClick function for navigating to direct messages

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -130,6 +130,10 @@ function Profile({ username }) {
   }, []);
 
 
+  const handleDMClick = () => {
+    navigate(`/direct-messages/${userInfo.uid}`);
+  };
+
   useEffect(() => {
     // Define the function to fetch user profile
     const getUser = async () => {
@@ -276,7 +280,7 @@ function Profile({ username }) {
               <button className="btn bioBtn" >ADD</button>
             </div>
             <div className="col-auto" style={{ paddingRight: "0px" }}>
-              <button className="btn bioBtn" >DM</button>
+              <button className="btn bioBtn" onClick={handleDMClick} >DM</button>
             </div>
           </div>
         )}
